7aab10590b6cb9a9fdac25e9496c4fc28445bb0f
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-05 07:19:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+//declaring the variables and assingning
+const companyName = 'Scuber';//this  should be const
+let mostProfitableNeighborhood='Chelsea';//this should be let
+let companyCeo='Susan Smith';//this should be let
